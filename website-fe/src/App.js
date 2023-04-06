fb36d84cb02a59ca46e9d44da6957224fd0b560a
mmoord <maxwellmoord@gmail.com>
2023-04-06 11:15:51 -0400

    Started a framework for the website with, about, blog, contact, and portfolio pages

--- website-fe/src/App.js
+++ website-fe/src/App.js
@@ -2,6 +2,12 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./components/homepage/home.jsx";
 import Navbar from './components/navbar/navbar';
+import About from './components/aboutme/about.jsx';
+import Contact from './components/contactpage/contact';
+import Blog from './components/blogpage/blog';
+import Portfolio from './components/portfoliopage/portfolio';
+
+
 
 function App() {
   return (
@@ -10,6 +16,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/blog" element={<Blog/>} />
+          <Route path="/portfolio" element={<Portfolio/>} />
+
         </Routes>
       </BrowserRouter>
     </>
---
+++ website-fe/src/components/aboutme/about.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+  return( 
+  <>
+  
+  
+  <h1>About</h1>
+
+
+
+
+</>
+  )
+}
